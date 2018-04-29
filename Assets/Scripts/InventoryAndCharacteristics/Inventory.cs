e1024f61a1932fb8cbf62998404c3af4c45fdade
Гаврилов Артем <dede13@yandex.ru>
2018-04-29 10:25:03 +0300

    Revert "Еще немного копаний в инвентаре"
    This reverts commit abafae44fa6766c123e65212b77cbaae339c4342.

--- Assets/Scripts/InventoryAndCharacteristics/Inventory.cs
+++ Assets/Scripts/InventoryAndCharacteristics/Inventory.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour {
 
-
-    List<Item> item;
 	// Use this for initialization
 	void Start () {
-        item = new List<Item>();
+		
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/InventoryAndCharacteristics/Item.cs
+++ Assets/Scripts/InventoryAndCharacteristics/Item.cs
@@ -5,12 +5,17 @@ public enum Type {Everything, Weapon, Armor, Food, Other, QuestItems};
 
 public class Item : MonoBehaviour {
 
-    public string name;
-    public int id;
-    public int countItem;
-    public bool isStackable;
-    [Multiline(5)]
+    public Sprite icon;
     public string description;
-    
 
+        // Use this for initialization
+    void Start () {
+
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
