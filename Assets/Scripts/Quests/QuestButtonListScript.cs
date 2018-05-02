a85b80e3bbaa2fb8d7491a937fa41f6f76f252cd
Гаврилов Артем <dede13@yandex.ru>
2018-05-02 09:38:39 +0300

    Добавлены скрипты для работы с Заданиями.

---
+++ Assets/Scripts/Quests/QuestButtonListScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestButtonListScript : MonoBehaviour {
+
+    public Button buttonQuest;
+    public Image LineImage;
+
+	// Use this for initialization
+	void Start () {
+        /*for(int i = 0; i < GetComponentsInChildren<QuestsButtonScript>().Length; i++)
+        {
+            transform.GetChild(i).GetComponentInChildren<QuestsButtonScript>().txt.text = "Задание " + (i+1).ToString();
+            if (i == 3)
+            {
+                LineImage = Instantiate(LineImage, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                LineImage.transform.parent = gameObject.transform;
+            }
+        }*/
+
+        int idLine = 0;
+        for (int i = 0; i < 7; i++)
+        {
+            var but = Instantiate(buttonQuest, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            but.transform.parent = gameObject.transform;
+            transform.GetChild(i + idLine).GetComponentInChildren<QuestsButtonScript>().txt.text = "Задание " + (i + 1).ToString();
+            if(i > 3)transform.GetChild(i + idLine).GetComponentInChildren<QuestsButtonScript>().txt.color = Color.gray;
+            if (i == 3)
+            {
+                LineImage = Instantiate(LineImage, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                LineImage.transform.parent = gameObject.transform;
+                idLine = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Quests/QuestsButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestsButtonScript : MonoBehaviour {
+
+    bool isActiveQuest;
+    int idQuest;
+    bool ClickButton;
+
+    public Image img;
+    public Text txt;
+
+    private void Awake()
+    {
+        isActiveQuest = true;
+        idQuest = 0;
+        ClickButton = false;
+        img.enabled = false;
+        txt.text = "123";
+    }
+
+
+}
