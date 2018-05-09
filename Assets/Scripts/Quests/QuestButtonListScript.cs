33b57cb7e07ec7c79b1aa63a6440ef6f202ba938
Гаврилов Артем <dede13@yandex.ru>
2018-05-09 17:37:23 +0300

    Add Quests.

--- Assets/Scripts/Quests/QuestButtonListScript.cs
+++ Assets/Scripts/Quests/QuestButtonListScript.cs
@@ -3,35 +3,108 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*TODO List:
+* Добавить все задания к персонажу в скрипт.
++ Изменить переменную актив на инт с 3 значениями: не получен/активен/пройдено
++ Добавить к кнопкам еще три поля для описания задания, что делать и тд
+* Сделать цикл по поиску сначала активных заданий, после этого нарисовать линию, а потом уже пройденные задания.
+*/
+
+
 public class QuestButtonListScript : MonoBehaviour {
 
     public Button buttonQuest;
     public Image LineImage;
+    public Text Discription1;
+    public Text Discription2;
+    public Text Discription3;
+    public PlayerScript QuestsList;
 
-	// Use this for initialization
-	void Start () {
-        /*for(int i = 0; i < GetComponentsInChildren<QuestsButtonScript>().Length; i++)
+    private int PositionLine;
+    
+    private void Awake()
+    {
+        PositionLine = 0;
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < QuestsList.QuestList.Count; i++)
         {
-            transform.GetChild(i).GetComponentInChildren<QuestsButtonScript>().txt.text = "Задание " + (i+1).ToString();
-            if (i == 3)
+            if (QuestsList.QuestList[i].isActiveQuest == 1)
             {
-                LineImage = Instantiate(LineImage, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-                LineImage.transform.parent = gameObject.transform;
+                var but = Instantiate(buttonQuest, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                but.transform.parent = gameObject.transform;
+                but.GetComponent<QuestsButtonScript>().txt.text = QuestsList.QuestList[i].txt;
+                but.GetComponent<QuestsButtonScript>().idQuest = QuestsList.QuestList[i].idQuest;
+                but.GetComponent<QuestsButtonScript>().img.enabled = QuestsList.QuestList[i].chooseimg > 0;
+                //but.onClick.AddListener(ClickButtonQuest());
+                but.onClick.AddListener(delegate { ClickButtonQuest(but.GetComponent<QuestsButtonScript>().idQuest - 1); });
+                PositionLine++;
             }
-        }*/
+        }
 
-        int idLine = 0;
-        for (int i = 0; i < 7; i++)
+        LineImage = Instantiate(LineImage, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        LineImage.transform.parent = gameObject.transform;
+
+        for (int i = 0; i < QuestsList.QuestList.Count; i++)
         {
-            var but = Instantiate(buttonQuest, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-            but.transform.parent = gameObject.transform;
-            transform.GetChild(i + idLine).GetComponentInChildren<QuestsButtonScript>().txt.text = "Задание " + (i + 1).ToString();
-            if(i > 3)transform.GetChild(i + idLine).GetComponentInChildren<QuestsButtonScript>().txt.color = Color.gray;
-            if (i == 3)
+            if (QuestsList.QuestList[i].isActiveQuest == 2)
             {
-                LineImage = Instantiate(LineImage, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-                LineImage.transform.parent = gameObject.transform;
-                idLine = 1;
+                var but = Instantiate(buttonQuest, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                but.transform.parent = gameObject.transform;
+                but.GetComponent<QuestsButtonScript>().txt.text = QuestsList.QuestList[i].txt;
+                but.GetComponent<QuestsButtonScript>().idQuest = QuestsList.QuestList[i].idQuest;
+                but.GetComponent<QuestsButtonScript>().img.enabled = QuestsList.QuestList[i].chooseimg > 0;
+                but.GetComponent<QuestsButtonScript>().txt.color = Color.gray;
+                but.onClick.AddListener(delegate { ClickButtonQuest(but.GetComponent<QuestsButtonScript>().idQuest - 1); });
+            }
+        }
+        for (int i = 0; i < QuestsList.QuestList.Count; i++)
+        {
+            if (QuestsList.QuestList[i].chooseimg == 1)
+            {
+                Discription1.text = QuestsList.QuestList[i].description1;
+                Discription2.text = QuestsList.QuestList[i].description2;
+                Discription3.text = QuestsList.QuestList[i].description3;
+            }
+        }
+    }
+
+    public void ClickButtonQuest(int idQ)
+    {
+        Quest v = QuestsList.QuestList[idQ];
+        v.chooseimg = 2;
+        QuestsList.QuestList[idQ] = v;
+        for (int i = 0; i < QuestsList.QuestList.Count; i++)
+        {
+            if (QuestsList.QuestList[i].chooseimg == 1)
+            {
+                Quest v10 = QuestsList.QuestList[i];
+                v10.chooseimg = 0;
+                QuestsList.QuestList[i] = v10;
+            }
+        }
+        for (int i = 0; i < QuestsList.QuestList.Count; i++)
+        {
+            if (QuestsList.QuestList[i].chooseimg == 2)
+            {
+                Quest v21 = QuestsList.QuestList[i];
+                v21.chooseimg = 1;
+                QuestsList.QuestList[i] = v21;
+                Discription1.text = QuestsList.QuestList[i].description1;
+                Discription2.text = QuestsList.QuestList[i].description2;
+                Discription3.text = QuestsList.QuestList[i].description3;
+            }
+        }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i != PositionLine) { 
+                if (QuestsList.QuestList[transform.GetChild(i).GetComponentInChildren<QuestsButtonScript>().idQuest - 1].chooseimg == 1)
+                {
+                    transform.GetChild(i).GetComponentInChildren<QuestsButtonScript>().img.enabled = true;
+                }
+                else transform.GetChild(i).GetComponentInChildren<QuestsButtonScript>().img.enabled = false;
             }
         }
     }
