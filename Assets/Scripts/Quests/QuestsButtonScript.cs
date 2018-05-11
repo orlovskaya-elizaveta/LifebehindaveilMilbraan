7abcef2e48e01a1e87d3c0fb47ede20b03e5b825
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-05-11 10:58:03 +0300

    Add Комментария к коду.

--- Assets/Scripts/Quests/QuestsButtonScript.cs
+++ Assets/Scripts/Quests/QuestsButtonScript.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestsButtonScript : MonoBehaviour {
-    public int idQuest;
+    public int idQuest; //Номер кнопки(квеста). Для работы со скриптом по вызову нажатие кнопки.
 
-    public Image img;
-    public Text txt;
-
-    private void Awake()
-    {
-        
-    }
+    public Image img; //Указатель на выбранный квест
+    public Text txt; //Текст кнопки
 }
