68a85e99a3f971106bd27a5a73457a8b30a9cab5
Гаврилов Артем <dede13@yandex.ru>
2018-05-26 14:14:26 +0300

    Добавлена анимация для врага.

--- Assets/Scripts/Creatures/EnemyBehaviour.cs
+++ Assets/Scripts/Creatures/EnemyBehaviour.cs
@@ -7,6 +7,8 @@ public class EnemyBehaviour : MonoBehaviour {
     private EnemyData enemyData;
     private UserData userData;
 
+    private Animator animator; //работа с анимацией
+
     float stayTime = 1;
 
 
@@ -24,9 +26,21 @@ public class EnemyBehaviour : MonoBehaviour {
 
     private UnityEngine.UI.Text HPInfo;
 
+    private EnemyState State //Установка и получение состояния (анимации)
+    {
+        get
+        {
+            return (EnemyState)animator.GetInteger("StateEnemy");
+        }
+        set
+        {
+            animator.SetInteger("StateEnemy", (int)value);
+        }
+    }
+
     private void Awake()
     {
-
+        animator = GetComponent<Animator>();
         //получаем всякие ссылки
         rigidbody = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
@@ -49,8 +63,8 @@ public class EnemyBehaviour : MonoBehaviour {
             double rand = Random.Range(0, 2 * (float)System.Math.PI);
             target[i] = new Vector3((transform.position.x + rad * (float)System.Math.Sin(rand)), (transform.position.y + rad * (float)System.Math.Cos(rand)), -2);
         }
-        currTarget = target[0];
-
+        //currTarget = target[0];
+        GetNextPoint();
     }
 
 
@@ -75,13 +89,75 @@ public class EnemyBehaviour : MonoBehaviour {
     {
         //если близко к ГГ, то бьем его
         Vector3 dir = GGPos - transform.position;
-        if (Vector3.Distance(transform.position, GGPos) <= 0.7f)
+        float x = GGPos.x - transform.position.x;
+        float a;
+        if (x != 0) a = Mathf.Atan((GGPos.y - transform.position.y) / (x)) * 180 / 3.14f;
+        else a = 90;
+        if (x >= 0 && a >= 0)
         {
+
+        }
+        else if (x <= 0 && a <= 0)
+        {
+            a = 180 + a;
+        }
+        else if (x <= 0 && a >= 0)
+        {
+            a = 180 + a;
+        }
+        else if (x >= 0 && a <= 0)
+        {
+            a = 360 + a;
+        }
+        //было 0.7
+        if (Vector3.Distance(transform.position, GGPos) <= 0.6f)
+        {
+            //Для смены анимации у НПС. Считаем угол от НПС до следующей точки
+            //Подумать еще с условиями (где-то лишние больше или равно)
+            if (a >= 315 || a <= 45)
+            {
+                sprite.flipX = false;
+                State = EnemyState.EnemyStateAttackRight;
+                
+            }
+            else if (a >= 45 && a <= 135)
+            {
+                State = EnemyState.EnemyStateAttackBack;
+            }
+            else if (a >= 135 && a <= 225)
+            {
+                sprite.flipX = false;
+                State = EnemyState.EnemyStateAttackLeft;
+            }
+            else if (a >= 225 && a <= 315)
+            {
+                State = EnemyState.EnemyStateAttackFront;
+            }
             Attack();
         }
         //если далеко - идем к нему
         else
         {
+            //Для смены анимации у НПС. Считаем угол от НПС до следующей точки
+            //Подумать еще с условиями (где-то лишние больше или равно)
+            if (a >= 315 || a <= 45)
+            {
+                sprite.flipX = false;
+                State = EnemyState.EnemyStateRight;                
+            }
+            else if (a >= 45 && a <= 135)
+            {
+                State = EnemyState.EnemyStateBack;
+            }
+            else if (a >= 135 && a <= 225)
+            {
+                sprite.flipX = true;
+                State = EnemyState.EnemyStateRight;
+            }
+            else if (a >= 225 && a <= 315)
+            {
+                State = EnemyState.EnemyStateFront;
+            }
             transform.Translate(dir.normalized * enemyData.stats.Get(Stats.Key.SPEED) * Time.deltaTime, Space.World);
         }
         
@@ -106,6 +182,45 @@ public class EnemyBehaviour : MonoBehaviour {
     {
         //просто движемся до выбранной точки, если дошли - меняем точку
         Vector3 dir = currTarget - transform.position;
+        float x = currTarget.x - transform.position.x;
+        float a;
+        if (x != 0) a = Mathf.Atan((currTarget.y - transform.position.y) / (x)) * 180 / 3.14f;
+        else a = 90;
+        if (x >= 0 && a >= 0)
+        {
+
+        }
+        else if (x <= 0 && a <= 0)
+        {
+            a = 180 + a;
+        }
+        else if (x <= 0 && a >= 0)
+        {
+            a = 180 + a;
+        }
+        else if (x >= 0 && a <= 0)
+        {
+            a = 360 + a;
+        }
+
+        if (a >= 315 || a <= 45)
+        {
+            State = EnemyState.EnemyStateRight;
+            sprite.flipX = false;
+        }
+        else if (a >= 45 && a <= 135)
+        {
+            State = EnemyState.EnemyStateBack;
+        }
+        else if (a >= 135 && a <= 225)
+        {
+            State = EnemyState.EnemyStateRight;
+            sprite.flipX = true;
+        }
+        else if (a >= 225 && a <= 315)
+        {
+            State = EnemyState.EnemyStateFront;
+        }
         transform.Translate(dir.normalized * enemyData.stats.Get(Stats.Key.SPEED) * Time.deltaTime, Space.World);
         if (Vector3.Distance(transform.position, currTarget) <= 0.3f)
         {
@@ -117,6 +232,48 @@ public class EnemyBehaviour : MonoBehaviour {
     {
         //выбираем новую точку, до которой будем двигаться
         currTarget = target[Random.Range(0, 5)];
+
+        //Для смены анимации у НПС. Считаем угол от НПС до следующей точки
+        //Подумать еще с условиями (где-то лишние больше или равно)
+        float x = currTarget.x - transform.position.x;
+        float a;
+        if (x != 0) a = Mathf.Atan((currTarget.y - transform.position.y) / (x)) * 180 / 3.14f;
+        else a = 90;
+        if (x >= 0 && a >= 0)
+        {
+
+        }
+        else if (x <= 0 && a <= 0)
+        {
+            a = 180 + a;
+        }
+        else if (x <= 0 && a >= 0)
+        {
+            a = 180 + a;
+        }
+        else if (x >= 0 && a <= 0)
+        {
+            a = 360 + a;
+        }
+
+        if (a >= 315 || a <= 45)
+        {
+            State = EnemyState.EnemyStateRight;
+            sprite.flipX = false;
+        }
+        else if (a >= 45 && a <= 135)
+        {
+            State = EnemyState.EnemyStateBack;
+        }
+        else if (a >= 135 && a <= 225)
+        {
+            State = EnemyState.EnemyStateRight;
+            sprite.flipX = true;
+        }
+        else if (a >= 225 && a <= 315)
+        {
+            State = EnemyState.EnemyStateFront;
+        }
     }
 
     void OnTriggerStay2D(Collider2D col)
@@ -149,4 +306,15 @@ public class EnemyBehaviour : MonoBehaviour {
 
 
 
+}
+
+public enum EnemyState
+{
+    EnemyStateRight, //0
+    EnemyStateFront, //1
+    EnemyStateBack, //2
+    EnemyStateAttackRight, //3
+    EnemyStateAttackFront, //4
+    EnemyStateAttackBack, //5
+    EnemyStateAttackLeft
 }
