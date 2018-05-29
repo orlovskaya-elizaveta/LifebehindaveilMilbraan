acad5a37291052e39bb207af188ae6a8838a4777
elizaveta <abovethesky@yandex.ru>
2018-05-29 23:51:42 +0300

    Добавлена минимальная смена дня и ночи. Ускорить течение суток можно уменьшив переменную timer в инспекторе главной камеры. Материал с шейдером, реагирующим на свет висит только на фоне, так что свет работает только на него.

---
+++ Assets/LightController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    float time;
+    bool isDay = true;
+
+    Light GlobalLight;
+    Light LocalLight;
+    public float timer = 1f;
+
+    void Start () {
+
+        GlobalLight = gameObject.transform.GetChild(0).GetComponentInChildren<Light>();
+        LocalLight = gameObject.transform.GetChild(1).GetComponentInChildren<Light>();
+        GlobalLight.intensity = 340;
+        LocalLight.intensity = 0;
+        //StartCoroutine(TestCoroutine());
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (isDay && timer <= 0)
+        {
+            GlobalLight.intensity -= 1;
+            LocalLight.intensity += 0.1f;
+            timer = 1;
+            if (GlobalLight.intensity == 0) isDay = false;
+        }
+        if (!isDay && timer <= 0)
+        {
+            GlobalLight.intensity += 1;
+            LocalLight.intensity -= 0.1f;
+            timer = 1;
+            if (GlobalLight.intensity == 340) isDay = true;
+        }
+
+    }
+
+    /*
+    IEnumerator TestCoroutine()
+    {
+        for (int i = 0; i <= 340; i++)
+        {
+            GlobalLight.intensity -= 1;
+            LocalLight.intensity += 0.1f;
+        }
+
+        for (int i = 0; i <= 340; i++)
+        {
+            GlobalLight.intensity += 1;
+            LocalLight.intensity -= 0.1f;
+        }
+
+
+
+
+
+        while (true)
+        {
+
+
+            yield return new WaitForSeconds(1);
+            //Debug.Log(Time.deltaTime);
+        }
+
+    }*/
+
+}
