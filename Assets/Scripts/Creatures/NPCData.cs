854b94f2e59ba9b89000b4f2ca2536fa18141c6c
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-06-06 11:34:35 +0300

    Update NPCData.cs

--- Assets/Scripts/Creatures/NPCData.cs
+++ Assets/Scripts/Creatures/NPCData.cs
@@ -10,7 +10,7 @@ public class NPCData : CreatureData
 
     public NPCData(string NPCname)
     {
-        questID = -1;//нет квеста
+        /*questID = -1;//нет квеста
         //TODO вынести чтение файла в NPCController
         name = NPCname;
         System.IO.StreamReader file = new System.IO.StreamReader("NPC_QuestID_Dialog.txt", System.Text.Encoding.GetEncoding(1251));
@@ -24,6 +24,28 @@ public class NPCData : CreatureData
                 dialog = file.ReadLine();
             }
         }
-        file.Close();
+        file.Close();*/
+        
+        //Загружаем из ресурсов наш xml файл
+        TextAsset xmlAsset = Resources.Load("NPC_QuestID_Dialog.xml");
+        // надо получить число элементов в root'овом теге.
+        XmlDocument xmlDoc = new XmlDocument();
+        if (xmlAsset) xmlDoc.LoadXml(xmlAsset.text);
+        
+        xmlNodeList dataList = xmlDoc.GetElementsByTagName("npc");
+        
+        foreach (XmlNode item in dataList) {
+            XmlNodeList itemContent = item.ChildNodes;
+            bool ThisNPC = false;
+            foreach (XmlNode itemItens in itemContent) {
+                if (itemItens.Name == NPCname) {
+                    name = NPCname;
+                    ThisNPC = true;
+                }
+                else if (itemItens.Name == "questID" && ThisNPC) questID = itemItens.InnerText; //TODO to int
+                else if (itemItens.Name == "dialog" && ThisNPC) dialog = itemItens.InnerText;
+            }
+        }
+        
     }
 }
