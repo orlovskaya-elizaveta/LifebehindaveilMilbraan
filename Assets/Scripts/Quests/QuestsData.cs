c6e5cff280eeb6101db4edeb16a7d41960f5d5b5
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-06-06 09:55:45 +0300

    Update QuestsData.cs

--- Assets/Scripts/Quests/QuestsData.cs
+++ Assets/Scripts/Quests/QuestsData.cs
@@ -44,7 +44,15 @@ public class QuestsData {
             QuestList.Add(newQuest);
         }
         file.Close();
-
+    }
+    
+    void CreateQuestsList()
+    {
+        //Считывание из xml
+        //Примеры из интернета:
+        //https://igroman14.livejournal.com/116218.html
+        //https://www.studica.com/blog/read-xml-file-in-unity
+        //http://unitynoobs.blogspot.com/2011/02/xml-loading-data-from-xml-file.html
     }
 
     //получить объект Quest по его id
